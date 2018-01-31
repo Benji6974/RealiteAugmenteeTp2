a889c421eece98de29e7fc300ecbfbe9d6946419
mgrange <martin.grange@etu.univ-lyon1.fr>
2018-01-31 15:29:42 +0100

    contour

--- main.cpp
+++ main.cpp
@@ -1,22 +1,16 @@
 #include "mainwindow.h"
-
 #include <OpenNI.h>
-
 #include <iostream>
-
 #include <cstddef>
-
+#include <vector>
 #include <opencv2/opencv.hpp>
-
-
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 #include <QApplication>
-
 using namespace openni;
 
-
-
-
+RNG rng(12345);
 
 int main(int argc, char **argv)
 {
@@ -96,28 +90,62 @@ int main(int argc, char **argv)
                for(int i = 0 ; i < h*w ; i++)
                {
                    imgBuf[i] = ((float) imgBuf[i] / UINT16_MAX) * 255;
+
                }
+
+//               ret,th = cv2.threshold(gray,127,255, 1);
+
 
 
                memcpy(frame.data, imgBuf, h*w*sizeof(uint16_t));
 
 
-
                                            // Copy the ir data from memory imgbuf -> frame.data
 
                                            // using memcpy (a string.h) function
-
                frame.convertTo(frame, CV_8U);
 
-                                           // OpenCV displays 8-bit data (I'm not sure why?)
 
-                                           // So, convert from 16-bit to 8-bit
+
+               int thresh = 100;
+               std::vector<std::vector<cv::Point> > contours;
+               std::vector<Vec4i> hierarchy;
+               double th = cv::threshold(frame, frame, 127, 255, 1);
+                cv::erode(frame, frame, Mat());
+                cv::dilate(frame, frame, Mat());
+                cv::Mat canny_output;
+               /// Detect edges using canny
+               Canny( frame, canny_output, thresh, thresh*2, 3 );
+               /// Find contours
+               findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+               /// Draw contours
+               Mat drawing = Mat::zeros( canny_output.size(), CV_8U);
+
+               int sizemax = 0;
+               int imax = 0;
+
+               for( int i = 0; i< contours.size(); i++ )
+                  {
+//                    if(contours.size()>sizemax){
+//                        sizemax = contours.size();
+//                        imax = i;
+//
+               //}
+
+                   Scalar color = Scalar(255,255,255);
+                   drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+
+                  }
+
+
+
 
                namedWindow("ir", 1);       // Create a named window
 
-               imshow("ir", frame);        // Show the IR VideoFrame in this window
+               imshow("ir", drawing);        // Show the IR VideoFrame in this window
 
-               char key = waitKey(10);
+               char key = cv::waitKey(10);
 
                if(key==27) break;          // Escape key number
 
@@ -133,8 +161,19 @@ int main(int argc, char **argv)
 
    ir.destroy();
 
-   device.close();                         // Close the PrimeSense Device
+   device.close();          // Close the PrimeSense Device
 
-    return app.exec();
+   cv::destroyAllWindows();
+
+
+   return app.exec();
+
+
+
 
 }
+
+
+
+
+
