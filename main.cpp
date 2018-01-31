8b6176d953381ec3d51a49cd8c84a4fd88e125c3
mgrange <martin.grange@etu.univ-lyon1.fr>
2018-01-31 11:55:41 +0100

    affichage normaliser de la profondeur

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
 #include "mainwindow.h"
+
 #include <OpenNI.h>
+
 #include <iostream>
+
 #include <cstddef>
+
 #include <opencv2/opencv.hpp>
 
+
+
 #include <QApplication>
+
 using namespace openni;
+
+
+
 
 
 int main(int argc, char **argv)
 {
  QApplication app(argc, argv);
 
-    MainWindow window;
+    //MainWindow window;
 
-    window.show();
+    //window.show();
 
     Device device;        // Software object for the physical device i.e.
 
@@ -32,7 +42,18 @@ int main(int argc, char **argv)
 
    rc = device.open(openni::ANY_DEVICE); // Open the Device
 
-   rc = ir.create(device, SENSOR_IR);    // Create the VideoStream for IR
+
+    /*
+   std::cout << "VIDEO MODE" << std::endl;
+   for(int i = 0 ; i < device.getSensorInfo(SENSOR_DEPTH)->getSupportedVideoModes().getSize() ; i++)
+   {
+       std::cout << "X : " << device.getSensorInfo(SENSOR_DEPTH)->getSupportedVideoModes()[i].getResolutionX() << " Y : " << device.getSensorInfo(SENSOR_DEPTH)->getSupportedVideoModes()[i].getResolutionY() << std::endl;
+   }
+   */
+
+   rc = ir.create(device, SENSOR_DEPTH);    // Create the VideoStream for IR
+
+   ir.setVideoMode(device.getSensorInfo(SENSOR_DEPTH)->getSupportedVideoModes()[5]);
 
    rc = ir.start();                      // Start the IR VideoStream
 
@@ -44,7 +65,7 @@ int main(int argc, char **argv)
 
    {
 
-       if(device.getSensorInfo(SENSOR_IR) != NULL)
+       if(device.getSensorInfo(SENSOR_DEPTH) != NULL)
 
        {
 
@@ -58,7 +79,7 @@ int main(int argc, char **argv)
 
                                            // This includes its resolution, fps and stream format.
 
-               const uint16_t* imgBuf = (const uint16_t*)irf.getData();
+               uint16_t* imgBuf = (uint16_t*)irf.getData();
 
                                            // PrimeSense gives the IR stream as 16-bit data output
 
@@ -66,11 +87,21 @@ int main(int argc, char **argv)
 
                w=irf.getWidth();
 
-               frame.create(h, w, CV_16U); // Create the OpenCV Mat Matrix Class Object
+               frame.create(h, w, CV_16UC1); // Create the OpenCV Mat Matrix Class Object
 
                                            // to receive the IR VideoFrames
+               //std::cout << imgBuf[500] << std::endl;
+
+
+               for(int i = 0 ; i < h*w ; i++)
+               {
+                   imgBuf[i] = ((float) imgBuf[i] / UINT16_MAX) * 255;
+               }
+
 
                memcpy(frame.data, imgBuf, h*w*sizeof(uint16_t));
+
+
 
                                            // Copy the ir data from memory imgbuf -> frame.data
 
@@ -107,8 +138,3 @@ int main(int argc, char **argv)
     return app.exec();
 
 }
-
-
-
-
-
